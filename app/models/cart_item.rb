17dad7d8c26db73e4ae02d44387a60c243f8190f
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-02 13:05:16 +0530

    Added model level validation and formatting

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,26 @@
 class CartItem < ApplicationRecord
+    MINIMUM_QUANTITY=1
     belongs_to :cart
     belongs_to :product
+    validate :validate_quantity
+
+    def validate_quantity
+        if quantity < MINIMUM_QUANTITY
+            cart_items.errors.add(:quantity, "Quantity cannot be less than 1")
+        end
+    end
+
+    def format
+        {
+            id: self.id,
+            cart_id: self.cart.id,
+            created_at: created_at,
+            quantity: quantity,
+            product:{
+                id: product.id,
+                name:product.name,
+                price: product.price
+            }
+        }
+    end
 end
