2fa5f2e00cded12d985e1f4a00a69edd0991c5f3
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-02 13:12:53 +0530

    Added endpoints related to order creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,34 @@ class User < ApplicationRecord
     
     after_create :create_cart
   
-    def create_cart
-      binding.pry
-      return if cart_exists?
-      Cart.create(user: self)
+    # def create_cart
+      
+    #   return if cart_exists?
+    #   Cart.create(user: self)
+    # end
+
+    # def cart_exists?
+    #   cart.present?
+    # end
+
+    def create_order!
+        cart_items=self.cart_items.includes(:product)
+        order=self.orders.new
+        if cart_items.blank?
+            order.errors.add(:cart,"should contain some item")
+            return order
+        end
+        order.save
+        cart_items.each do |cart_item|
+            order.order_items.create(
+                product_id:cart_item.product_id,
+                quantity:cart_item.quantity,
+                product_name: cart_item.product.name,
+                product_price:cart_item.product.price
+            )
+            cart_item.destroy
+        end  
+        return order 
     end
 
-    def cart_exists?
-      cart.present?
-    end
 end
