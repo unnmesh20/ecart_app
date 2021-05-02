3d53ffd54363a22a8337077d505d3bbf3daa3dc4
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-02 13:03:40 +0530

    Optimized the endpoint code for creating and made changes in endpoint of show all orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,55 +1,36 @@
 module Api
     class OrdersController < ApplicationController
         def show
-            # all_orders = current_user.orders.find_by(user_id:current_user.id)
             specific_order=current_user.orders.find_by(id: params[:id])
             if specific_order.nil?
                 render json: {"master_says":"No such orders found"}
             else
-
-                specific_order_details=specific_order.order_items
-                render json: {
-                    order_id:specific_order.id,
-                    payment_status:specific_order.payment_status,
-                    invoice_number:specific_order.invoice_number,
-                    order_details:specific_order_details
-
-                }
+                render json: specific_order.format
             end
-
         end
         
         def index
-            all_orders = current_user.orders.where(user_id:current_user.id)
-
-            render json: all_orders
-            
+            all_orders = current_user.orders
+            # render json: all_orders
+            data=[]
+            all_orders.each do |single_order|
+                data << single_order.format
+            end  
+            render json: data
         end
         
         def create
-            cart_items_list=current_user.cart.cart_items
-            order_creation = Order.create(user_id:current_user.id,payment_status:"yes",invoice_number: rand(1000...100000))
-            adding_item_to_order_list = cart_items_list.each do |item|
-                order_creation.order_items.create(
-                    product_id: item.product_id,
-                    quantity: item.quantity
-                )
+            order=current_user.create_order!
+            if order.errors.blank?
+                render json:{
+                    "master_says":"Order successfully placed.",
+                     order: order.format
+
+                }, status: :ok
+            else
+                render json: {"master says":"No item found in the cart", error: order.errors}
             end
-            # #  binding.pry
-            # @current_user_cart_item=current_user.cart.cart_items.where(cart_id:current_user.cart)
-            # @current_user_cart_item.destroy
-            # render json: adding_item_to_order_list
-
-
-
-        end
-
-        private
-        def fetch_order
-            @order=current_user.orders.find params[:order_id]
-        end
-
-
+        end 
     
     end
     
