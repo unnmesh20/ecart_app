1595c8ba97001d15da2d194a3684de602fb5d2e4
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-02 13:07:59 +0530

    Changes done related to formatting

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,19 @@
 class OrderItem < ApplicationRecord
     belongs_to :order
     belongs_to :product
+    
+    def format
+        {
+            id:self.id,
+            quantity:self.quantity,
+            price: self.quantity*self.product_price,
+            product:{
+                id:self.product_id,
+                product_name:self.product_name,
+                price:self.product_price
+
+            }
+        }
+    end
+
 end
