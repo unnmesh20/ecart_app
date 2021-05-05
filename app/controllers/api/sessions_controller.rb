0c10e4d6efeec6b849454818b3d482b24276f69f
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-06 00:27:10 +0530

    Added JWT authentication for a user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -6,6 +6,8 @@ module Api
           email: params[:email]
         ).first
 
+        #binding.pry
+
         if user.blank?
           render json:{
             "master says":"Invalid autentication",
@@ -15,16 +17,19 @@ module Api
           
   
         if user.valid_password?(params[:password])
-          token = user.generate_token!
+          # token = user.generate_token!
+          token = encode_token({id: user.id})
+          user.update(token:token,token_created_at:Time.now)
           render json: {user: {
                           id: user.id,
                           email: user.email,
-                          name: user.name
+                          name: user.name,
+                          token: token
                         }}, status: :ok
         else
           render json: {status: 401, error: true, message: 'Unauthorized'}, status: :unauthorized
         end
-
       end
+
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,45 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
+  JWT_SECRET_KEY = Rails.application.secrets.secret_key_base
+  JWT_ALGORITH = 'HS512'
+
   before_action :authenticate!
 
   def authenticate!
+    # binding.pry
     authenticate_or_request_with_http_token do |token, options|
-      @current_user = User.validate_token!(token)
-    end
-  rescue Exception => e
-    data = {}
-    if e.is_a?(User::InvalidToken)
-      data = {
-        status: 'invalid_token',
-        message: 'invalid token'
-      }
-    elsif e.is_a?(User::ExpiredToken)
-      data = {
-        status: 'expired_token',
-        message: 'Expired token'
-      }
+      payload = decode_token(token)
+      @current_user = User.find payload['id']
     end
 
-    render json: data, status: :unauthorized
+  rescue JWT::ExpiredSignature
+    data = {
+      status: 'expired_token',
+      message: 'Expired token'
+    }
+
+    render json: data, status: :unauthorized and return
+  rescue JWT::ImmatureSignature, JWT::DecodeError
+    data = {
+      status: 'invalid_token',
+      message: 'Invalid Token from Immature Signature'
+    }
+
+    render json: data, status: :unauthorized and return
   end
 
   def current_user
     @current_user
+  end
+
+  def encode_token(payload, exp = 10.seconds.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, JWT_SECRET_KEY, JWT_ALGORITH)
+  end
+  
+  def decode_token(token)
+    JWT.decode(token, JWT_SECRET_KEY, true, algorithm: JWT_ALGORITH).first
   end
 
 end
