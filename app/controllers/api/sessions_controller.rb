7b49b66a935cd859cf53c8503adb5796cde68c5b
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-05 19:52:32 +0530

    Added token based verification

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,20 +1,30 @@
 module Api
     class SessionsController < ApplicationController
+      skip_before_action :authenticate!
       def create
         user = User.where(
-          email: params[:email],
-          password: params[:password]
+          email: params[:email]
         ).first
+
+        if user.blank?
+          render json:{
+            "master says":"Invalid autentication",
+            success:false
+          }, status: :unauthorized and return
+        end
+          
   
-        if user.present?
+        if user.valid_password?(params[:password])
+          token = user.generate_token!
           render json: {user: {
                           id: user.id,
                           email: user.email,
                           name: user.name
-                        }}
+                        }}, status: :ok
         else
-          render json: {status: 401, error: true, message: 'Unauthorized'}
+          render json: {status: 401, error: true, message: 'Unauthorized'}, status: :unauthorized
         end
+
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,31 @@
 class ApplicationController < ActionController::API
-    include ActionController::HttpAuthentication::Token::ControllerMethods
-  
-     before_action :authenticate!  
-     
-    def authenticate!
-      authenticate_or_request_with_http_token do |token, options|
-        @current_user = User.where(id: token).first
-        @current_user.present?
-      end
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate!
+
+  def authenticate!
+    authenticate_or_request_with_http_token do |token, options|
+      @current_user = User.validate_token!(token)
     end
-  
-    def current_user
-      @current_user
+  rescue Exception => e
+    data = {}
+    if e.is_a?(User::InvalidToken)
+      data = {
+        status: 'invalid_token',
+        message: 'invalid token'
+      }
+    elsif e.is_a?(User::ExpiredToken)
+      data = {
+        status: 'expired_token',
+        message: 'Expired token'
+      }
     end
+
+    render json: data, status: :unauthorized
   end
+
+  def current_user
+    @current_user
+  end
+
+end
