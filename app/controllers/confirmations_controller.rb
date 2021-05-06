c58c5f5f3849d8f18928c02446e4677d8a85855e
Unnmesh Mishra <unnmeshmishra@gmail.com>
2021-05-06 20:07:44 +0530

    Cleaned the code and added the show method for displaying message after email verification

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,20 +3,13 @@ class ConfirmationsController < Devise::ConfirmationsController
     def show
         self.resource = resource_class.confirm_by_token(params[:confirmation_token])
         # sign_in(resource)
-        render json: {status: "The email id is being confirmed. You can login now!!"}
+        render json:{
+            "master says": "Your email is confirmed. You can now sign in"
+        }, status: :ok
         
     end
-    def send_reconfirmation_instructions
-        binding.pry
-        @reconfirmation_required = false
 
-        unless @skip_confirmation_notification
-          send_confirmation_instructions
-        end
-    end
-    private
-    def after_confirmation_path_for(resource_name,resource)
-        #sign_in(resource)
-        # render json: {status: "Done"}
-    end
+    # def send_reconfirmation_instructions
+    # end
+    
 end
